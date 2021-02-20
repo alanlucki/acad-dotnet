9ceac9783de017e4519deb1b8854147c201dac99
alan.lucki@gmail.com <alan.lucki@gmail.com>
2021-02-19 20:16:20 -0500

    add [ 01 ] hello-world

---
+++ [ 01 ] hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace __01___hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Beep();
+        }
+    }
+}
